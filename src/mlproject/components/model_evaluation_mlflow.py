f9283d8b26cc9c9aaf2b7e9fb1b94de866ca9849
prabhav131 <guptaprabhav131@gmail.com>
2024-05-30 12:05:57 +0530

    added prediction pipeline and flask app

--- src/mlproject/components/model_evaluation_mlflow.py
+++ src/mlproject/components/model_evaluation_mlflow.py
@@ -1,13 +1,15 @@
 import os
+import numpy as np
 import tensorflow as tf
 from pathlib import Path
 import mlflow
 import mlflow.keras
 from urllib.parse import urlparse
-
+from mlflow.models.signature import infer_signature
 from mlproject.entity.config_entity import EvaluationConfig
 from mlproject.utils.common import save_json
-
+import time
+from mlproject import logger
 
 class Evaluation:
     def __init__(self, config: EvaluationConfig):
@@ -63,6 +65,24 @@ class Evaluation:
             os.environ["MLFLOW_TRACKING_USERNAME"] = "prabhav131"
             os.environ["MLFLOW_TRACKING_PASSWORD"] = "7a57c6a16d8bdede4119a82de1fd4e2fb08645e6"
             
+            # # specifying model signature to be able to sotre the model in mlflow model registry
+            input_example = np.random.rand(1, 224, 224, 3).astype(np.float32)
+            
+            start_time = time.time()
+            # Code to measure
+            output_example = self.model.predict(input_example)
+            
+            end_time = time.time()
+
+            execution_time = end_time - start_time
+            logger.info(f">>>>>> time to executedummy predict function for inferring signature is {execution_time} seconds <<<<<<")
+
+            # input_example = np.random.rand(1, 224, 224, 3).astype(np.float32)
+            # output_example = self.model.predict(input_example)
+            # Infer the signature
+            signature = infer_signature(input_example, output_example)
+            
+            
             # Set tracking URIs
             mlflow.set_tracking_uri(self.config.mlflow_uri)
             
@@ -85,19 +105,29 @@ class Evaluation:
             
             # Start a new MLflow run
             with mlflow.start_run():
+                
+                # # log the model with inferrred signature
+                # mlflow.keras.log_model(self.model, artifact_path="model", signature=signature)
+                # # Register the model
+                # model_uri = f"runs:/{mlflow.active_run().info.run_id}/model"
+                # mlflow.register_model(model_uri=model_uri, name="VGG16Model")
+                # logger.info(f">>>>>>> registered model to model registry <<<<<<")
+                
                 # Log parameters and metrics
                 if isinstance(self.config.all_params, dict):
                     mlflow.log_params(self.config.all_params)
+                    logger.info(f">>>>>>> logged experiment params <<<<<<")
                 else:
                     raise ValueError("all_params should be a dictionary")
                     
                 if len(self.score) == 2:
                     mlflow.log_metrics({"loss": self.score[0], "accuracy": self.score[1]})
+                    logger.info(f">>>>>>> logged experiment metrics <<<<<<")
                 else:
                     raise ValueError("score should contain exactly 2 elements: loss and accuracy")
                 
+            logger.info(f">>>>>>> completed mlflow run <<<<<<")    
                 
-
         except mlflow.exceptions.MlflowException as e:
             print(f"MLflowException: {e}")
             raise
